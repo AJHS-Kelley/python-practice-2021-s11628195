c0b2f565b333ec43531ecb0cbf9a4e4261026cc0
Amari Mayfield <70582293+s11628195@users.noreply.github.com>
2021-10-29 14:37:55 -0400

    Update Variables_practice.py

--- Variables_practice.py
+++ Variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Amari Mayfield, 10/29/2021 2:22, version 0.0
+# Variables Practice, Amari Mayfield, 10/29/2021 2:37, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 13 # INTEGER, any +/- WHOLE number and 0
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal 
+death_cry = "Curse you, Zagreous! I hate you!"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
